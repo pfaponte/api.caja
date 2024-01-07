9d954e4235da1b58692a2915a0128bbaf9f51039
Pedro <fernandoaponte1103@gmail.com>
2024-01-07 11:17:27 -0500

    Feature: Agregar entidades

---
+++ src/main/java/ec/edu/espam/api/caja/domain/Movimiento.java
@@ -0,0 +1,45 @@
+package ec.edu.espam.api.caja.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity(name = "movimiento")
+public class Movimiento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "fecha")
+    @NotNull(message = "Fecha es requerida")
+    private LocalDate fecha;
+
+    @Column(name = "tipo")
+    @NotNull(message = "Tipo es requerido")
+    @Enumerated(EnumType.STRING)
+    private TipoMovimiento tipo;
+
+    @Column(name = "monto")
+    @NotNull(message = "Monto es requerido")
+    private BigDecimal monto;
+
+    @Column(name = "balance")
+    @NotNull(message = "Balance es requerido")
+    private BigDecimal balance;
+
+    @NotNull(message = "Cuenta es requerido")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Cuenta cuenta;
+
+    public enum TipoMovimiento {
+        DEBITO, CREDITO;
+    }
+}
---
+++ src/main/java/ec/edu/espam/api/caja/domain/Persona.java
@@ -0,0 +1,43 @@
+package ec.edu.espam.api.caja.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "identificacion")
+    @NotEmpty(message = "Identificación es requerida")
+    private String identificacion;
+
+    @Column(name = "nombre")
+    @NotEmpty(message = "Nombre es requerido")
+    private String nombre;
+
+    @Column(name = "genero")
+    @NotEmpty(message = "Género es requerido")
+    private String genero;
+
+    @Column(name = "direccion")
+    @NotEmpty(message = "Dirección es requerida")
+    private String direccion;
+
+    @Column(name = "telefono")
+    @NotEmpty(message = "Teléfono es requerido")
+    private String telefono;
+
+    @Column(name = "edad")
+    @NotNull(message = "Edad es requerida")
+    private Integer edad;
+}
