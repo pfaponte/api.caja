6139c89d8b21d8d32360aadbee8f45171c56d754
Pedro <fernandoaponte1103@gmail.com>
2024-01-07 10:15:43 -0500

    Feature: Inicio de proyecto

---
+++ src/test/java/ec/edu/espam/api/caja/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espam.api.caja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
